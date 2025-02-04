f37c6d84dc0b8c8ded62c37e1479e48d1da432ad
Kira <kirillbutakov02021994@gmail.com>
2025-02-04 09:21:53 +0300

    homework

--- my_project/config/settings.py
+++ my_project/config/settings.py
@@ -135,3 +135,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_URL = '/users/login/'
+
+# Настройки почтового сервиса
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
